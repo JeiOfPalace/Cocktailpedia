6246f3086afa2aeccf191d0a35971dd0ee63c30b
JeiOfPalace <59843057+JeiOfPalace@users.noreply.github.com>
2021-05-26 21:59:29 +0200

    Segon push

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { Tab4Page } from './tab4.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab3PageRoutingModule } from './tab4-routing.module';
+import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 @NgModule({
   imports: [
@@ -15,7 +15,7 @@ import { Tab3PageRoutingModule } from './tab4-routing.module';
     FormsModule,
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab4Page }]),
-    Tab3PageRoutingModule,
+    Tab4PageRoutingModule,
   ],
   declarations: [Tab4Page]
 })
